6fbac01d9c069385655860fd1f43b1a277b529ff
moonkop <moonkop@qq.com>
2017-12-10 01:07:11 +0800

    prevent 502bad on dom

--- code502.js
+++ code502.js
@@ -1,12 +1,14 @@
 function detect502() {
-
-    return false;
+	return $("center")
+		.eq(0)
+		.text()
+		.startsWith("50");
 }
 
-$(document).ready(function () {
-    if (detect502) {
-        setTimeout(() => {
-            window.location.href = "";
-        }, 3000);
-    }
+$(document).ready(function() {
+	if (detect502()) {
+		setTimeout(() => {
+			window.location.href = "";
+		}, 3000);
+	}
 });
