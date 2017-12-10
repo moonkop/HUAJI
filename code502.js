31ee32b98bf60a31de9da713ff14feea88cf02ea
moonkop <moonkop@qq.com>
2017-12-10 23:42:40 +0800

    fix bugs add test domains

--- code502.js
+++ code502.js
@@ -17,12 +17,23 @@ $(document).ready(function () {
     detect502Timer = setInterval(function () {
         reloadIf502();
     }, 10000);
-    myTimerArray.push(detect502Timer);
+    //myTimerArray.push(detect502Timer);
 });
 function reloadIf502() {
     if (detect502()) {
         setTimeout(() => {
             window.location.href = "";
         }, 3000);
+    }
+}
+
+function ClearAllTimers() {
+    // clearInterval(4); //停止系统自带上传进度计时器
+    // clearInterval(1); 				//停止焦点状态检测计时器
+    // clearInterval(3); 		//停止时间记录计时器
+
+    for (var i = 0; i < 100; i++) {
+
+        clearInterval(i);
     }
 }
--- common.js
+++ common.js
@@ -15,16 +15,7 @@ function getUserInfo() {
 var myTimerArray = [];
 
 
-function ClearAllTimers() {
-    // clearInterval(4); //停止系统自带上传进度计时器
-    // clearInterval(1); 				//停止焦点状态检测计时器
-    // clearInterval(3); 		//停止时间记录计时器
 
-    for (var i = 0; i < 100; i++) {
-
-        clearInterval(i);
-    }
-}
 
 function sendToBackgroud(data, pageName) {
     data.UserId = userid;
